f1b4c83f0e8cf5126aa44cda3ace26c65ea495e7
Igor Polegato <igorpolegato@gmail.com>
2022-07-26 18:59:32 -0300

    CountBot finalizado

--- bot/data.py
+++ bot/data.py
@@ -3,9 +3,26 @@ import configparser
 config = configparser.ConfigParser()
 config.read("./config/config.ini")
 
+ignorar = []
+usuarios = []
+
 bt = config['bot']
+lk = config['links']
+msgens = config['mensagens']
 
 api_id = bt['api_id']
 api_hash = bt['api_hash']
 bot_token = bt['bot_token']
 bot_name = bt['name']
+
+for l in lk['ignorar'].split(","):
+    ignorar.append(l.strip())
+
+for u in msgens['users'].split(","):
+    usuarios.append(u.strip())
+
+lk_ignore = ignorar
+
+perm_users = map(int, usuarios)
+gp_id = int(msgens['grupo'])
+mx_range = int(msgens['range'])
